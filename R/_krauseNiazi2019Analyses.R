2619f995847251c17135e83c31e3bdc9fef593e0
Adnan Niazi <adnaniazi@gmail.com>
2019-05-09 12:39:11 +0200

    renamed functions back

--- R/_krauseNiazi2019Analyses.R
+++
@@ -1,5 +0,0 @@
-# # Here's a good place to put your top-level package documentation
-# .onLoad <- function (lib, pkgname = "krauseNiazi2019Analyses") {
-#   ## Put stuff here you want to run when your package is loaded
-#   invisible()
-# }
