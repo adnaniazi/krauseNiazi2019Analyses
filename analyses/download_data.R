0ae33493b92b35518a092fa341ef2efc84e03a60
Adnan Niazi <adnaniazi@gmail.com>
2019-05-09 13:39:47 +0200

    updated packrat

--- analyses/download_data.R
+++ analyses/download_data.R
@@ -1,5 +1,3 @@
-drake::expose_imports('krauseNiazi2019Analyses')
-
 dna_ff_bc =
   get_data(release_version = 'v1.0.0',
            file_name = 'rna-krause-rna002-flipflop_basecalling-tailfindr_estimates.csv')
